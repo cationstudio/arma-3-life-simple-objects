0e9af5d364707c7bba0475800ac3007a1f9020c9
Julian Bauer <julian.bauer@cationstudio.com>
2021-12-30 21:10:14 +0100

    Update functions.cpp description

--- objects/functions.cpp
+++ objects/functions.cpp
@@ -1,3 +1,10 @@
+/*
+    File: functions.cpp
+    Author: Julian Bauer (julian.bauer@cationstudio.com)
+
+    Description:
+    Function config for simple objects system. 
+*/
 class Cation_Objects {
     tag = "cat_objects";
     class functions {
